a5dd7c5b5684b2c1d2db369d1e767c9610cf5e17
Rafael de Souza Freire <rafaeldesouzafreire@pop-os.localdomain>
2022-09-16 14:41:32 -0300

    List of Games

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { View, Image } from "react-native";
-
+import { View, Image, FlatList } from "react-native";
+import { GAMES } from "../../utils/games";
 import { styles } from "./styles";
 import LogoImg from "../../assets/logo-nlw-esports.png";
 import { Heading } from "../../components/Heading";
+import { GamesCard } from "../../components/GamesCard";
 
 export function Home() {
   return (
@@ -12,6 +13,13 @@ export function Home() {
       <Heading
         title="Encontre seu duo!"
         subtitle="Selecione o jogo que deseja jogar..."
+      />
+      <FlatList
+        data={GAMES}
+        renderItem={({ item }) => <GamesCard data={item} />}
+        showsHorizontalScrollIndicator={false}
+        horizontal
+        contentContainerStyle={styles.contentList}
       />
     </View>
   );
